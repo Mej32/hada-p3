a86d40230db3eada5aca20754735563baec3c554
mqg7 <mqg@alu.ua.es>
2024-03-26 00:53:56 +0100

    48766914Z Añadidos metodos en EN y CAD Products

--- hada-p3/library/CADProduct.cs
+++ hada-p3/library/CADProduct.cs
@@ -17,6 +17,25 @@ namespace library
         {
             return false;
         }
+        public bool Update(ENProduct en){
+        
+        }/* Actualiza los datos de un producto en la BDconlos datos del producto representado por el parámetro en.*/
+        
+        public bool Delete(ENProduct en){
+        
+        } /*Borra el producto representado en en de la BD.*/
+        public bool Read(ENProduct en){
 
+        }/* Devuelve solo el producto indicado leído de la BD.*/
+        public bool ReadFirst(ENProduct en){
+
+        } /*Devuelve solo el primer producto de la BD.*/
+        public bool ReadNext(ENProducten){
+    
+        }/*Devuelvesolo el producto siguiente  al indicado.*/
+        public bool ReadPrev(ENProduct en){
+
+        } /*Devuelve solo elproducto anterior   al indicado.*/
+        
     }
 }
--- hada-p3/library/ENProduct.cs
+++ hada-p3/library/ENProduct.cs
@@ -32,5 +32,24 @@ namespace library
         {
             return true;
         }
+
+        public bool Update() { 
+        
+        } /*Actualiza este producto en la BD.Para ello hará uso de los métodos apropiados de CADProduct.Devuelve false si no se ha podido realizar la operación.*/
+        public bool Delete() { 
+        
+        } /*Borra este producto de la BD.Para ello hará uso de los métodos apropiados de CADProduct.Devuelve false si no se ha podido realizar la operación.*/
+        public bool Read() { 
+        
+        } /*Recupera el producto indicado de la BD.Para ello hará uso de los métodos apropiados de CADProduct.Devuelve false si no se ha podido realizar la operación.*/
+        public bool ReadFirst() { 
+        
+        } /*Recupera todos los productos de la BD y devuelve solo el primer producto.Para ello hará uso de los métodos apropiados de CADProduct.Devuelve false si no se ha podido realizar la operación.*/
+        public bool ReadNext() { 
+        
+        } /*Recupera todos los productos de la BD ydevuelve solo el producto siguiente al indicado.Para ello hará uso de los métodos apropiados de CADProduct.Devuelve false si no se ha podido realizar la operación.*/
+        public bool ReadPrev() { 
+        
+        } /*Recupera todos los productos de la BD ydevuelvesolo el producto anterior al indicado.Para ello hará uso de los métodos apropiados de CADProduct.Devuelve false si no se ha podido realizar la operación.*/
     }
 }
