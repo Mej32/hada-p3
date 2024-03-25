59626e6a1ca95cc3fda1ecc79e3b1d1a7bf3d846
jbn23 <jbn23@alu.ua.es>
2024-03-25 12:13:49 +0100

    50387249E, Creada interfaz de la página web

--- hada-p3/proWeb/Default.aspx
+++ hada-p3/proWeb/Default.aspx
@@ -1,5 +1,31 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="proWeb.WebForm1" %>
+﻿<%@ Page Title="HADA P3" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="proWeb.WebForm1" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Products management</h1>
+    <p> <!-- Creación de la interfaz de la página web -->
+        Code <asp:TextBox ID ="Codebox" runat ="server"></asp:TextBox>
+
+    </p>
+    <p>
+        Name <asp:TextBox ID ="NameBox" runat ="server"></asp:TextBox>
+    </p>
+    <p>
+        Amount <asp:TextBox ID ="AmountBox" runat ="server"></asp:TextBox>
+    </p>
+    <p>
+        Category  <asp:DropDownList ID ="CategoryList" runat ="server">
+                    <asp:ListItem Text="Computing" Value="0"></asp:ListItem>
+                    <asp:ListItem Text="Telephony" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="Gaming" Value="2"></asp:ListItem>
+                    <asp:ListItem Text="Home appliances" Value="3"></asp:ListItem>
+                  </asp:DropDownList>
+    </p>
+    <p>
+        Price <asp:TextBox ID="PriceBox" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Creation Date <asp:TextBox ID="CreationDateBox" runat="server"></asp:TextBox>
+    </p>
+    <asp:Button Text="Create" ID="CreateButton" runat="server" Height="22px" Width="109px"></asp:Button>
 </asp:Content>
--- hada-p3/proWeb/Default.aspx.designer.cs
+++ hada-p3/proWeb/Default.aspx.designer.cs
@@ -13,5 +13,68 @@ namespace proWeb
 
     public partial class WebForm1
     {
+
+        /// <summary>
+        /// Control Codebox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Codebox;
+
+        /// <summary>
+        /// Control NameBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox NameBox;
+
+        /// <summary>
+        /// Control AmountBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox AmountBox;
+
+        /// <summary>
+        /// Control CategoryList.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList CategoryList;
+
+        /// <summary>
+        /// Control PriceBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox PriceBox;
+
+        /// <summary>
+        /// Control CreationDateBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox CreationDateBox;
+
+        /// <summary>
+        /// Control CreateButton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button CreateButton;
     }
 }
