63d8bc07ec99eb9ca7219b42fec3cec486cfff57
jbn23 <jbn23@alu.ua.es>
2024-03-25 17:49:17 +0100

    50387249E,Creados botones y metodos de evento de estos en aspx.cs

--- hada-p3/proWeb/Default.aspx.cs
+++ hada-p3/proWeb/Default.aspx.cs
@@ -13,5 +13,35 @@ namespace proWeb
         {
 
         }
+        protected void Create_click(object sender, EventArgs create)
+        {
+            //llama a crear obj
+        }
+        protected void Update_click(object sender, EventArgs update)
+        {
+            //llama a actualizar obj
+        }
+        protected void Delete_click(object sender, EventArgs delete)
+        {
+            //llama a borrar obj
+        }
+        protected void Read_click(object sender, EventArgs read)
+        {
+            //llama a leer obj
+        }
+        protected void ReadFirst_click(object sender, EventArgs RF)
+        {
+            //llama a leer primer obj
+        }
+        protected void ReadPrev_click(object sender, EventArgs RP)
+        {
+            //llama a leer obj anterior obj
+
+        }
+        protected void ReadNext_click(object sender, EventArgs RN)
+        {
+            //llama a leer siguiente obj
+
+        }
     }
 }
